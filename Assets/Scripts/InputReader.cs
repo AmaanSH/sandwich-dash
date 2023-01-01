b7fd78b79c3fd6a3222d40d4d32c0934ad227dad
Amaan Shawkath <amaanshaw@live.co.uk>
2023-01-01 01:58:45 +0000

    adding input reader

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public class InputReader : MonoBehaviour, Controls.IPlayerActions
+{
+    public Vector2 MovementValue { get; private set; }
+
+    public event Action InteractEvent;
+
+    private Controls controls;
+
+    private void Start()
+    {
+        controls = new Controls();
+        controls.Player.SetCallbacks(this);
+
+        controls.Player.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        controls.Player.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MovementValue = context.ReadValue<Vector2>();
+    }
+
+    public void OnInteraction(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            InteractEvent?.Invoke();
+        }
+    }
+}
